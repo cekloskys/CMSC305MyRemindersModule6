0d2e8ad56faf33d556ef4b40888bee6eb93c5154
Sue Ceklosky <cekloskys@chc.edu>
2022-11-13 14:18:12 -0500

    Module 6 Assignment

---
+++ src/navigation/Router.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../screens/Home';
+import TabNavigator from './TabNavigator';
+import AddReminder from '../screens/AddReminder';
+import AddPriority from '../screens/AddPriority';
+
+const Stack = createStackNavigator();
+
+const Router = props => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={'Home'}
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name={'Get Reminders!'} component={TabNavigator}/>
+        <Stack.Screen name={'Add Reminder'} component={AddReminder}/>
+        <Stack.Screen name={'Add Priority'} component={AddPriority}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
